63cb8729270f28d2a01f500aa9bd58a20962bbf9
Ian <iantennyson@gmail.com>
2017-03-31 08:45:57 +0100

    added some of my classes and functions

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,8 +17,19 @@ twentyfive_ml.save()
 fifty_ml = Measure.new({ 'ml' => '50' })
 fifty_ml.save()
 
-ingredient1 = Ingredient.new({ 'name' => 'vodka', 'measure_id' => fifty_ml.id, 'price' => '17', 'is_alcoholic' => true })
+ingredient1 = Ingredient.new({ 'name' => 'Vodka', 'measure_id' => fifty_ml.id, 'price' => '15', 'is_alcoholic' => true })
 ingredient1.save()
+
+ingredient2 = Ingredient.new({ 'name' => 'Gin', 'measure_id' => fifty_ml.id, 'price' => '17', 'is_alcoholic' => true })
+ingredient2.save()
+
+ingredient3 = Ingredient.new({ 'name' => 'Rum', 'measure_id' => fifty_ml.id, 'price' => '19', 'is_alcoholic' => true })
+ingredient3.save()
+
+ingredient4 = Ingredient.new({ 'name' => 'Tequila', 'measure_id' => fifty_ml.id, 'price' => '20', 'is_alcoholic' => true })
+ingredient4.save()
+
+
 
 binding.pry
 nil
