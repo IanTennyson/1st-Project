1b1df377713130aec41b3a0d0029e8d29cc94a5c
Ian <iantennyson@gmail.com>
2017-04-01 16:15:37 +0100

    updated table is now working about to write update methods

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,25 +24,25 @@ fifty_ml.save()
 
 
 
-ingredient1 = Ingredient.new({ 'name' => 'Vodka', 'measure_id' => fifty_ml.id, 'price_per_ltr' => 15.0, 'is_alcoholic' => true, 'quantity' => 7 })
+ingredient1 = Ingredient.new({ 'name' => 'Vodka', 'measure_id' => fifty_ml.id, 'price_per_ltr' => 15.0, 'is_alcoholic' => true, 'quantity' => 7, 'cost_price' => 0.0, 'sale_price' => 0.0, 'profit' => 0.0 })
 ingredient1.save()
 
-ingredient2 = Ingredient.new({ 'name' => 'Gin', 'measure_id' => fifty_ml.id, 'price_per_ltr' => 15.0, 'is_alcoholic' => true, 'quantity' => 5  })
+ingredient2 = Ingredient.new({ 'name' => 'Gin', 'measure_id' => fifty_ml.id, 'price_per_ltr' => 15.0, 'is_alcoholic' => true, 'quantity' => 5, 'cost_price' => 0.0, 'sale_price' => 0.0, 'profit' => 0.0   })
 ingredient2.save()
 
-ingredient3 = Ingredient.new({ 'name' => 'Rum', 'measure_id' => fifty_ml.id, 'price_per_ltr' => 15.0, 'is_alcoholic' => true, 'quantity' => 10  })
+ingredient3 = Ingredient.new({ 'name' => 'Rum', 'measure_id' => fifty_ml.id, 'price_per_ltr' => 15.0, 'is_alcoholic' => true, 'quantity' => 10, 'cost_price' => 0.0, 'sale_price' => 0.0, 'profit' => 0.0   })
 ingredient3.save()
 
-ingredient4 = Ingredient.new({ 'name' => 'Tequila', 'measure_id' => fifty_ml.id, 'price_per_ltr' => 28.58, 'is_alcoholic' => true, 'quantity' => 4  })
+ingredient4 = Ingredient.new({ 'name' => 'Tequila', 'measure_id' => fifty_ml.id, 'price_per_ltr' => 28.58, 'is_alcoholic' => true, 'quantity' => 4, 'cost_price' => 0.0, 'sale_price' => 0.0, 'profit' => 0.0   })
 ingredient4.save()
 
-ingredient5 = Ingredient.new({ 'name' => 'Orange', 'measure_id' => fifty_ml.id, 'price_per_ltr' => 1.23, 'is_alcoholic' => false, 'quantity' => 10 })
+ingredient5 = Ingredient.new({ 'name' => 'Orange', 'measure_id' => fifty_ml.id, 'price_per_ltr' => 1.23, 'is_alcoholic' => false, 'quantity' => 10, 'cost_price' => 0.0, 'sale_price' => 0.0, 'profit' => 0.0  })
 ingredient5.save()
 
-ingredient6 = Ingredient.new({ 'name' => 'Cranberry', 'measure_id' => fifty_ml.id, 'price_per_ltr' => 1.98, 'is_alcoholic' => false, 'quantity' => 10 })
+ingredient6 = Ingredient.new({ 'name' => 'Cranberry', 'measure_id' => fifty_ml.id, 'price_per_ltr' => 1.98, 'is_alcoholic' => false, 'quantity' => 10, 'cost_price' => 0.0, 'sale_price' => 0.0, 'profit' => 0.0  })
 ingredient6.save()
 
-ingredient7 = Ingredient.new({ 'name' => 'Peach Schnapps', 'measure_id' => twentyfive_ml.id, 'price_per_ltr' => 7.50, 'is_alcoholic' => true, 'quantity' => 9  })
+ingredient7 = Ingredient.new({ 'name' => 'Peach Schnapps', 'measure_id' => twentyfive_ml.id, 'price_per_ltr' => 7.50, 'is_alcoholic' => true, 'quantity' => 9, 'cost_price' => 0.0, 'sale_price' => 0.0, 'profit' => 0.0 })
 ingredient7.save()
 
 
